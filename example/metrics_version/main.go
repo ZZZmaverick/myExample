7f545c245bfc5ef4367e0830d381d7aaca255f75
ZZZmaverick <66453377+ZZZmaverick@users.noreply.github.com>
2020-06-21 18:51:39 +0800

    Update main.go

--- example/metrics_version/main.go
+++ example/metrics_version/main.go
@@ -9,22 +9,23 @@ import (
 	"example/metrics"
 )
 
+// 设置两个处理函数，前者为index，后者为Handler函数（来自"github.com/prometheus/client_golang/prometheus/promhttp"）
 func main(){
 	http.HandleFunc("/abc", index)
 	http.Handle("/metrics", promhttp.Handler())
-	metrics.Register()
-	err := http.ListenAndServe(":5565", nil) // 设置监听的端口
+	metrics.Register()	
+	err := http.ListenAndServe(":5565", nil) // 设置监听，绑定在5565端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	timer:=metrics.NewAdmissionLatency()
-	metrics.RequestIncrease()
+	timer:=metrics.NewAdmissionLatency()	// 记录从访问路由到响应的延迟
+	metrics.RequestIncrease()	//请求次数加一
 	num:=os.Getenv("Num")
 	if num==""{
-		Fibonacci(10)
+		Fibonacci(10)	//延时
 		_,err:=w.Write([]byte("there is no env Num. Computation successed\n"))
 		if err!=nil{
 			log.Println("err:"+err.Error()+" No\n")
